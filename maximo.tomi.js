0c112d2e80a66de641aa5910ad5a75f846dd9cb5
tomivm <tomivm98@gmail.com>
2020-10-07 20:42:57 -0300

    borre mis archivos viejos

--- maximo.tomi.js
+++
@@ -1,8 +0,0 @@
-//https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Math/max
-a=[1,2,3,5,6,4,7,8,9];
-
-function getMaxOfArray(a) {
-  return Math.max.apply(null, a);
-}
-
-console.log(getMaxOfArray(a))
